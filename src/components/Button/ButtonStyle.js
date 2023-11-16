56386e49ca83596444e98cfb55d6d866248a9ea1
vadik184 <133372327+vadik184@users.noreply.github.com>
2023-11-16 22:19:31 +0200

    Update ButtonStyle.js

--- src/components/Button/ButtonStyle.js
+++ src/components/Button/ButtonStyle.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const ButtonStyled = styled('button')`
+  width: 30px;
   padding: 8px 16px;
   border-radius: 2px;
   background-color: #3f51b5;
@@ -19,6 +20,7 @@ export const ButtonStyled = styled('button')`
   min-width: 180px;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  margin: 0 auto;
   &:hover {
     background-color: #303f9f;
     &:focus {
